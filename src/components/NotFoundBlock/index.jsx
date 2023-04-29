16ea571276b476187e87d5a27191b7948df3cf8c
StanKzchnko <49488521+StanislavKozachenko@users.noreply.github.com>
2023-04-29 16:24:06 +0300

    Delete src directory

--- src/components/NotFoundBlock/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-import styles from './NotFoundBlock.module.scss';
-
-export default function NotFoundBlock() {
-  return <h1 className={styles.root}>Ничего не найдено</h1>;
-}
